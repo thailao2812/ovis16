59d48fb830c40b0667be2f1233edf5f89ad81a7a
thai lao <thai.lao@sucden.com>
2024-11-15 10:09:30 +0700

    update vietnam

--- vietnam_sucden/sd_report_vietnam/models/fob_weight_franchise.py
+++ vietnam_sucden/sd_report_vietnam/models/fob_weight_franchise.py
@@ -55,42 +55,16 @@ class FOB_Franchise(models.Model):
                 pp.id AS product_id,
                 sil.name AS description,
                 MAX(si.total_line_qty) AS qty_si,
-                SUM(deli.gdn_quantity) AS ex_store_qty,
+                MAX(si.gdn_qty) AS ex_store_qty,
                 MAX(si.invoice_qty) AS invoice_qty,
-                MAX(si.total_line_qty) - SUM(deli.gdn_quantity) AS franchise_qty,
-                ((MAX(si.total_line_qty) - SUM(deli.gdn_quantity)) / MAX(si.total_line_qty)) * 100 AS franchise
+                MAX(si.total_line_qty) - MAX(si.gdn_qty) AS franchise_qty,
+                ((MAX(si.total_line_qty) - MAX(si.gdn_qty)) / MAX(si.total_line_qty)) * 100 AS franchise
             FROM
                 shipping_instruction si
             JOIN
                 s_contract sc ON sc.id = si.contract_id
             LEFT JOIN
                 sale_contract nvs ON si.id = nvs.shipping_id
-            LEFT JOIN (
-                SELECT
-                    dor.contract_id,
-                    SUM(dol.product_qty) AS do_quantity,
-                    SUM(sm.gdn_quantity) AS gdn_quantity,
-                    sp.state
-                FROM
-                    delivery_order dor
-                JOIN
-                    delivery_order_line dol ON dor.id = dol.delivery_id
-                LEFT JOIN
-                    stock_picking sp ON dor.picking_id = sp.id
-                LEFT JOIN (
-                    SELECT
-                        stock_move_line.picking_id,
-                        SUM(stock_move_line.init_qty) AS gdn_quantity
-                    FROM
-                        stock_move_line
-                    GROUP BY
-                        stock_move_line.picking_id
-                ) sm ON dor.picking_id = sm.picking_id
-                WHERE
-                    sp.state = 'done'
-                GROUP BY
-                    dor.contract_id, sp.state
-            ) deli ON nvs.id = deli.contract_id
             LEFT JOIN
                 stock_warehouse sw ON sw.id = si.warehouse_id
             JOIN
