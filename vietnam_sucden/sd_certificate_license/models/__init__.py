873d1578677914d3c9513353b05d0deeecfcfc38
Son Huynh <son.huynh@sucden.com>
2024-11-29 16:23:22 +0700

    Create new pivoted license certificate detail 2

--- vietnam_sucden/sd_certificate_license/models/__init__.py
+++ vietnam_sucden/sd_certificate_license/models/__init__.py
@@ -8,3 +8,4 @@ from . import sd_certificate_license_detail
 from . import sale_contract_line
 from . import delivery_order
 from . import lot_stack_allocation_license
+from . import sd_certificate_license_detail2
