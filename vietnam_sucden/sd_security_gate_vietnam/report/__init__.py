f38e3c7782bacd1122992d92880e354a589acc0b
thai lao <thai.lao@sucden.com>
2024-12-17 16:34:35 +0700

    update vietnam

--- vietnam_sucden/sd_security_gate_vietnam/report/__init__.py
+++ vietnam_sucden/sd_security_gate_vietnam/report/__init__.py
@@ -4,3 +4,4 @@ from . import printout_payment_npv_fix_npe_parser
 from . import printout_request_payment_npe_report_parse
 from . import printout_request_payment_ptbf_parse
 from . import printout_request_payment_ptbf_advance_parse
+from . import request_payment_ptbf_advance_parse
