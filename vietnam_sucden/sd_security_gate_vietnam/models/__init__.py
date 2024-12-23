0c9603be84f751e5ebb4f5bea174f1d03faf8466
thai lao <thai.lao@sucden.com>
2024-12-23 15:46:36 +0700

    update vietnam

--- vietnam_sucden/sd_security_gate_vietnam/models/__init__.py
+++ vietnam_sucden/sd_security_gate_vietnam/models/__init__.py
@@ -11,3 +11,4 @@ from . import advance_line, reference_information
 from . import stock_allocation
 from . import ir_attachment
 from . import fixation_for_advance_ptbf_npe
+from . import fixation_for_advance_npe_total
