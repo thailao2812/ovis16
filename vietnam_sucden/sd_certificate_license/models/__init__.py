4eab99f07e0ede6e3f96402a8ffae03de0bd833f
Son Huynh <son.huynh@sucden.com>
2025-01-08 10:12:31 +0700

    add license and cert field in NPE, Allocation Treeview; button_load cert list from SI to SVNS

--- vietnam_sucden/sd_certificate_license/models/__init__.py
+++ vietnam_sucden/sd_certificate_license/models/__init__.py
@@ -5,7 +5,7 @@ from . import purchase_contract
 from . import s_contract
 from . import shipping_instruction_license_allocation
 from . import sd_certificate_license_detail
-from . import sale_contract_line
+from . import sale_contract
 from . import delivery_order
 from . import lot_stack_allocation_license
 from . import sd_certificate_license_detail2
