8d304a0f09b611e0c26b12e36e23156ad5706c8d
Huỳnh Anh Sơn <son.huynh@sucden.com>
2025-03-07 15:49:50 +0700

    Add Area field to Zone and Stock Movement

--- vietnam_sucden/sd_report_vietnam/models/stock_lot.py
+++ vietnam_sucden/sd_report_vietnam/models/stock_lot.py
@@ -11,9 +11,4 @@ class StockLot(models.Model):
     _inherit = 'stock.lot'
     _order = 'name, id'
     
-    area_name_link = fields.Char(related='zone_id.area_name', string='Area', compute='_compute_area_name', store = True)
-
-    @api.depends('zone_id.area_name')
-    def _compute_area_name(self):
-        for lot in self:
-            lot.area_name = lot.zone_id.area_name
+    area_name_link = fields.Char(related='zone_id.area_name', string='Area', store = True)
