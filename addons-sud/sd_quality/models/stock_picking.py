d46abd4c80794eb1779443577918dcfe6fc9ee6f
Huỳnh Anh Sơn <son.huynh@sucden.com>
2025-05-09 11:11:46 +0700

    Update: quy trình Merge Stack -> quality for GIP

--- addons-sud/sd_quality/models/stock_picking.py
+++ addons-sud/sd_quality/models/stock_picking.py
@@ -273,8 +273,9 @@ class StockPicking(models.Model):
                                             WHERE request_kcs_line.x_warehouse_id is NULL; ''')
                 
             if pick.picking_type_id.code =='production_out':
+                pick.kcs_line.refresh()
                 pick.load_qc_gip()
-    
+
     def btt_approved(self):
         for pick in self:
             if not pick.kcs_line:
