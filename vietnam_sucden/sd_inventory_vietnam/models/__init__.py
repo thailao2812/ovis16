21c5c81e9e13d4d5a9376df09a84f0567c87acd2
Huỳnh Anh Sơn <son.huynh@sucden.com>
2025-05-28 16:43:40 +0700

    Add new: Module call API to pull IoT data

--- vietnam_sucden/sd_inventory_vietnam/models/__init__.py
+++ vietnam_sucden/sd_inventory_vietnam/models/__init__.py
@@ -2,3 +2,4 @@
 from . import stock_picking
 from . import stock_lot
 from . import stock_quant
+from . import iot_thinks
--- vietnam_sucden/sd_inventory_vietnam/wizard/__init__.py
+++ vietnam_sucden/sd_inventory_vietnam/wizard/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 from . import wizard_report_stack_merge
 from . import in_out_stack
+# from . import device_telemetry_wizard
