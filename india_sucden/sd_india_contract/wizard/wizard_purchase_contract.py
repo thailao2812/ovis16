3ec088d7ebb771294f7cdac6732ac663a4b84e52
thai lao <thai.lao@sucden.com>
2026-03-11 15:21:19 +0700

    update

--- india_sucden/sd_india_contract/wizard/wizard_purchase_contract.py
+++ india_sucden/sd_india_contract/wizard/wizard_purchase_contract.py
@@ -14,8 +14,10 @@ class WizardPurchaseContract(models.TransientModel):
     def _compute_price_per_bag(self):
         for rec in self:
             if rec.price_unit and rec.price_unit > 0:
-                packing_capacity = rec.purchase_contract_id.packing_id.capacity
-                rec.price_per_bag = rec.price_unit * packing_capacity
+                if rec.purchase_contract_id.product_id.template_qc == 'raw':
+                    rec.price_per_bag = rec.price_per_bag = rec.price_unit * 50
+                else:
+                    rec.price_per_bag = 0
 
     def button_convert(self):
         npe_nvp_relation = self.env['npe.nvp.relation']
